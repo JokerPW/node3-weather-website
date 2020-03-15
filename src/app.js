b3c5f41f4d3b2f51861bb8aba286102d8338233f
Renan Amaral <jokerpw@gmail.com>
2020-03-15 19:33:06 -0300

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 //nodemon src/app.js -e js,hbs
+//https://jokerpw-udemy-node-weather-app.herokuapp.com/
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
@@ -6,6 +7,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define Paths
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -96,7 +98,7 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(__dirname)
-    console.log('Server is up on port 3000!')
+    console.log('Server is up on port: ' + port)
 });
